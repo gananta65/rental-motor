5985f1e71c221c1e071f9c7eca0b86c1fb58e6c5
Gananta <gananta65@gmail.com>
2025-07-05 14:45:46 +0800

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,4 @@
+// middleware.ts
 import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
@@ -6,22 +7,11 @@ export async function middleware(req: NextRequest) {
   const res = NextResponse.next();
 
   const supabase = createMiddlewareClient({ req, res });
-
-  const {
-    data: { user },
-    error,
-  } = await supabase.auth.getUser();
-
-  console.log("🔐 Middleware user:", user);
-  console.log("🧾 Middleware error:", error);
-  console.log("🍪 Cookies:", req.cookies.getAll());
-  console.log("🌐 SUPABASE_URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
-  console.log("🌐 BASE_URL:", process.env.NEXT_PUBLIC_BASE_URL);
-  console.log("🌍 NODE_ENV:", process.env.NODE_ENV);
+  await supabase.auth.getUser(); // ini akan refresh session jika expired
 
   return res;
 }
 
 export const config = {
-  matcher: ["/admin/:path*"], // pastikan path sesuai
+  matcher: ["/admin/:path*"], // sesuaikan path yang ingin dilindungi
 };
